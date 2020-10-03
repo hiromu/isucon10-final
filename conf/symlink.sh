89542635f7ca015e37c9ddf5ba9eba15c12ca4e6
Hiromu Yakura <hiromu1996@gmail.com>
2020-10-03 02:18:30 +0000

    Add symlink.sh

---
+++ conf/symlink.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+DIR='/home/isucon/webapp/conf'
+
+ln -sf $DIR/sysctl.conf /etc/sysctl.conf
+ln -sf $DIR/envoy/config.yaml /etc/envoy/config.yaml
+ln -sf $DIR/systemd/xsuportal-api-golang.service /etc/systemd/system/xsuportal-api-golang.service
+ln -sf $DIR/systemd/xsuportal-api-nodejs.service /etc/systemd/system/xsuportal-api-nodejs.service
+ln -sf $DIR/systemd/xsuportal-api-ruby.service /etc/systemd/system/xsuportal-api-ruby.service
+ln -sf $DIR/systemd/xsuportal-api-rust.service /etc/systemd/system/xsuportal-api-rust.service
+ln -sf $DIR/systemd/xsuportal-web-golang.service /etc/systemd/system/xsuportal-web-golang.service
+ln -sf $DIR/systemd/xsuportal-web-nodejs.service /etc/systemd/system/xsuportal-web-nodejs.service
+ln -sf $DIR/systemd/xsuportal-web-ruby.service /etc/systemd/system/xsuportal-web-ruby.service
+ln -sf $DIR/systemd/xsuportal-web-rust.service /etc/systemd/system/xsuportal-web-rust.service
+ln -sf $DIR/mysql/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
